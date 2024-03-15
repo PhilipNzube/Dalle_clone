437c61a9cab8d535c65c1f6cc0b567e89a7c1382
PhilipNzube <Onwubaliliphilip@gmail.com>
2024-03-15 04:43:52 +0100

    Made a few changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,12 @@ import React, { useState, useEffect } from 'react'
 import { Loader, Card, FormField } from '../components';
 
 const RenderCards = ({ data, title }) => {
-  if (data?.length > 0) {
-    return data.map((post) => <Card key={post._id} {...post} />)
-  }
+  // if (data?.length > 0) {
+  //   return data.map((post) => <Card key={post._id} {...post} />);
+  // }
 
   return (
-    <h2 className='mt-5 font-bold text-[#6469ff] text-xl uppercase'>
+    <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">
       {title}
     </h2>
   )
@@ -17,7 +17,49 @@ const RenderCards = ({ data, title }) => {
 export default function Home() {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
-  const [searchText, setSearchText] = useState('')
+  const [searchText, setSearchText] = useState('');
+  const [searchResults, setSearchResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+
+        if (response.ok) {
+          const result = await response.json();
+
+          setAllPosts(result.data.reverse())
+        }
+      } catch (error) {
+        alert(error)
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResults = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+
+        setSearchResults(searchResults);
+      }, 500)
+    );
+  }
+
   return (
     <section className='max-w-7x1 mx-auto'>
       <div>
@@ -30,7 +72,14 @@ export default function Home() {
       </div>
 
       <div className='mt-16'>
-        <FormField />
+        <FormField
+          labelName="Search posts"
+          type="text"
+          name="text"
+          placeholder="Search posts"
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
       </div>
 
       <div className='mt-10'>
@@ -48,12 +97,12 @@ export default function Home() {
             <div className='grid lg:griid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
               {searchText ?
                 <RenderCards
-                  data={[]}
+                  data={searchResults}
                   title="No search results found"
                 />
                 :
                 <RenderCards
-                  data={[]}
+                  data={[allPosts]}
                   title="No posts found"
                 />
               }
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,3 +1,5 @@
+import { saveAs } from 'file-saver';
+
 import { surpriseMePrompts } from '../constants';
 
 export function getRandomPrompt(prompt) {
@@ -7,4 +9,8 @@ export function getRandomPrompt(prompt) {
     if (randomPrompt === prompt) return getRandomPrompt(prompt);
 
     return randomPrompt;
+}
+
+export async function downloadImage(_id, photo) {
+    saveAs(photo, `download-${_id}.jpg`); 
 }
